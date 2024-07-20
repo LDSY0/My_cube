519fc86a7d019ef0e72cd14dd469975126e83bc2
dgl <2433353370@qq.com>
2024-07-20 15:07:54 +0800

    finish moving and ui mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,63 @@
+use bevy::prelude::*;
+use bevy::transform::TransformSystem;
+use bevy::utils::Instant;
+use bevy_egui::EguiPlugin;
+use bevy_mod_picking::backends::raycast::bevy_mod_raycast::prelude::RaycastVisibility;
+use bevy_mod_picking::backends::raycast::RaycastBackendSettings;
+use bevy_mod_picking::DefaultPickingPlugins;
+use std::collections::VecDeque;
+
+use camera::*;
+use cube::*;
+use moving::*;
+use ui::*;
+
+mod camera;
+mod cube;
+mod moving;
+mod ui;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(EguiPlugin)
+        .add_plugins(DefaultPickingPlugins)
+        // .add_plugin(DebugCursorPickingPlugin)
+        // .add_plugin(DebugEventsPickingPlugin)
+        .add_systems(Startup, (setup_camera, setup_cube))
+        .insert_resource(RaycastBackendSettings {
+            require_markers: true,
+            raycast_visibility: RaycastVisibility::MustBeVisible,
+        })
+        .insert_resource(CubeSettings::default())
+        .insert_resource(SideMoveQueue(VecDeque::new()))
+        .insert_resource(MouseDraggingRecorder {
+            start_pos: None,
+            piece: None,
+        })
+        .insert_resource(MaintainingTimer(Instant::now()))
+        .register_type::<Piece>()
+        .add_event::<ScrambleEvent>()
+        .add_event::<ResetEvent>()
+        .add_systems(PreUpdate, (choose_movable_pieces,))
+        .add_systems(
+            Update,
+            (
+                rotate_cube,
+                game_ui,
+                scramble_cube,
+                reset_cube,
+                zoom_camera,
+                move_camera,
+            ),
+        )
+        .add_systems(
+            PostUpdate,
+            ((
+                piece_translation_round,
+                cleanup_movable_pieces.after(piece_translation_round),
+            )
+                .after(TransformSystem::TransformPropagate),),
+        )
+        .run();
 }
--- src/moving.rs
+++ src/moving.rs
@@ -1,14 +1,13 @@
 use crate::cube::*;
-use bevy::prelude::*; //导入 Bevy 引擎的核心和拾取模块。
+use bevy::prelude::*;
 use bevy_mod_picking::prelude::*;
-use std::collections::VecDeque; //导入双端队列
-use std::f32::consts::FRAC_PI_2; //导入常量 PI、TAU 和 FRAC_PI_2（π/2）
+use std::collections::VecDeque;
+use std::f32::consts::FRAC_PI_2;
 use std::f32::consts::PI;
 use std::f32::consts::TAU;
 
 #[derive(Debug, Clone, Copy)]
 pub enum SideRotation {
-    //定义了 90 度和 180 度的顺时针和逆时针旋转。
     Clockwise90,
     Clockwise180,
     Counterclockwise90,
@@ -16,7 +15,6 @@ pub enum SideRotation {
 
 #[derive(Debug, Clone, Copy)]
 pub enum Axis {
-    //定义了 X、Y、Z 三个轴。
     X,
     Y,
     Z,
@@ -24,27 +22,355 @@ pub enum Axis {
 
 #[derive(Debug, Clone, Copy, Event)]
 pub struct SideMoveEvent {
-    //定义了魔方的侧面旋转事件，包含旋转的面和旋转方向。
-    // 旋转的面，对应固定的x/y/z坐标值
+    // 对应x/y/z坐标值
     pub side: (Axis, f32),
-    // 旋转
     pub rotate: SideRotation,
 }
 
 #[derive(Debug, Resource)]
-pub struct SideMoveQueue(pub VecDeque<SideMoveEvent>); //包含一个 VecDeque 用于存储 SideMoveEvent
+pub struct SideMoveQueue(pub VecDeque<SideMoveEvent>);
 
 #[derive(Debug, Resource)]
 pub struct MouseDraggingRecorder {
-    //记录鼠标拖动开始的位置和选中的魔方块
     pub start_pos: Option<Vec3>,
     pub piece: Option<Entity>,
 }
 
 impl MouseDraggingRecorder {
-    //实现了 clear 方法，用于重置记录。
     pub fn clear(&mut self) {
         self.start_pos = None;
         self.piece = None;
+    }
+}
+
+pub fn handle_drag_start(
+    listener: Listener<Pointer<DragStart>>,
+    mut recorder: ResMut<MouseDraggingRecorder>,
+) {
+    println!("drag start event: {:?}", listener);
+    let piece_entity = listener.target;
+    recorder.piece = Some(piece_entity.clone());
+    recorder.start_pos = listener.hit.position;
+
+    info!("MouseDraggingRecorder started {:?}", recorder);
+}
+
+pub fn handle_move(
+    listener: Listener<Pointer<Move>>,
+    mut recorder: ResMut<MouseDraggingRecorder>,
+    mut side_move_queue: ResMut<SideMoveQueue>,
+    q_pieces: Query<&Transform, With<Piece>>,
+) {
+    if listener.event.hit.position.is_some() && recorder.start_pos.is_some() {
+        let start_pos = recorder.start_pos.unwrap();
+        let current_pos = listener.event.hit.position.unwrap();
+
+        // 鼠标拽动距离超过临界值
+        if start_pos.distance(current_pos) > 0.5 {
+            // 触发旋转
+            info!("Trigger side move event, end_pos: {:?}", current_pos);
+            let translation = q_pieces.get(recorder.piece.unwrap()).unwrap().translation;
+            let event = gen_side_move_event(translation,
+                 recorder.start_pos.unwrap(), current_pos);
+            info!("gen event: {:?}", event);
+            if event.is_some() {
+                side_move_queue.0.push_back(event.unwrap());
+            }
+
+            // 清除recorder
+            recorder.clear();
+        }
+    }
+}
+
+pub fn handle_drag_end(mut recorder: ResMut<MouseDraggingRecorder>) {
+    println!("drag end event");
+    recorder.clear();
+}
+
+pub fn rotate_cube(
+    mut movable_pieces: Query<(&mut MovablePiece, &mut Transform)>,
+    cube_settings: Res<CubeSettings>,
+    time: Res<Time>,
+) {
+    for (mut movable_piece, mut transform) in &mut movable_pieces {
+        let axis = match movable_piece.axis {
+            Axis::X => Vec3::X,
+            Axis::Y => Vec3::Y,
+            Axis::Z => Vec3::Z,
+        };
+        let mut angle = match movable_piece.rotate {
+            SideRotation::Clockwise90 | SideRotation::Clockwise180 => {
+                cube_settings.rotate_speed * TAU * time.delta_seconds()
+            }
+            SideRotation::Counterclockwise90 => {
+                -cube_settings.rotate_speed * TAU * time.delta_seconds()
+            }
+        };
+
+        let mut new_left_angle = movable_piece.left_angle - angle;
+        match movable_piece.rotate {
+            SideRotation::Clockwise90 | SideRotation::Clockwise180 => {
+                if new_left_angle < 0.0 {
+                    angle = movable_piece.left_angle;
+                    new_left_angle = 0.0;
+                }
+            }
+            SideRotation::Counterclockwise90 => {
+                if new_left_angle > 0.0 {
+                    angle = movable_piece.left_angle;
+                    new_left_angle = 0.0;
+                }
+            }
+        };
+
+        transform.rotate_around(Vec3::new(0.0, 0.0, 0.0), Quat::from_axis_angle(axis, angle));
+        movable_piece.left_angle = new_left_angle;
+    }
+}
+
+pub fn piece_translation_round(mut movable_pieces: Query<(&mut Transform, &MovablePiece)>) {
+    for (mut transform, movable_piece) in &mut movable_pieces {
+        if movable_piece.left_angle == 0.0 {
+            transform.translation.x = transform.translation.x.round();
+            transform.translation.y = transform.translation.y.round();
+            transform.translation.z = transform.translation.z.round();
+        }
+    }
+}
+
+fn gen_side_move_event(
+    piece_translation: Vec3,
+    start_pos: Vec3,
+    end_pos: Vec3,
+) -> Option<SideMoveEvent> {
+    if (start_pos.x.abs() - 1.5).abs() < 0.001 {
+        let delta_y = end_pos.y - start_pos.y;
+        let delta_z = end_pos.z - start_pos.z;
+        if delta_y.abs() > delta_z.abs() {
+            // y轴变化大，沿z轴旋转
+            let rotate = {
+                // 右面
+                if start_pos.x > 0.0 {
+                    if delta_y > 0.0 {
+                        SideRotation::Clockwise90
+                    } else {
+                        SideRotation::Counterclockwise90
+                    }
+                // 左面
+                } else {
+                    if delta_y > 0.0 {
+                        SideRotation::Counterclockwise90
+                    } else {
+                        SideRotation::Clockwise90
+                    }
+                }
+            };
+            return Some(SideMoveEvent {
+                side: (Axis::Z, piece_translation.z.round()),
+                rotate,
+            });
+        } else {
+            // z轴变化大，沿y轴旋转
+            let rotate = {
+                // 右面
+                if start_pos.x > 0.0 {
+                    if delta_z > 0.0 {
+                        SideRotation::Counterclockwise90
+                    } else {
+                        SideRotation::Clockwise90
+                    }
+                // 左面
+                } else {
+                    if delta_z > 0.0 {
+                        SideRotation::Clockwise90
+                    } else {
+                        SideRotation::Counterclockwise90
+                    }
+                }
+            };
+            return Some(SideMoveEvent {
+                side: (Axis::Y, piece_translation.y.round()),
+                rotate,
+            });
+        }
+    } else if (start_pos.y.abs() - 1.5).abs() < 0.001 {
+        let delta_x = end_pos.x - start_pos.x;
+        let delta_z = end_pos.z - start_pos.z;
+        if delta_x.abs() > delta_z.abs() {
+            // x轴变化大，沿z轴旋转
+            let rotate = {
+                // 上面
+                if start_pos.y > 0.0 {
+                    if delta_x > 0.0 {
+                        SideRotation::Counterclockwise90
+                    } else {
+                        SideRotation::Clockwise90
+                    }
+                // 下面
+                } else {
+                    if delta_x > 0.0 {
+                        SideRotation::Clockwise90
+                    } else {
+                        SideRotation::Counterclockwise90
+                    }
+                }
+            };
+            return Some(SideMoveEvent {
+                side: (Axis::Z, piece_translation.z.round()),
+                rotate,
+            });
+        } else {
+            // z轴变化大，沿x轴旋转
+            let rotate = {
+                // 上面
+                if start_pos.y > 0.0 {
+                    if delta_z > 0.0 {
+                        SideRotation::Clockwise90
+                    } else {
+                        SideRotation::Counterclockwise90
+                    }
+                // 下面
+                } else {
+                    if delta_z > 0.0 {
+                        SideRotation::Counterclockwise90
+                    } else {
+                        SideRotation::Clockwise90
+                    }
+                }
+            };
+            return Some(SideMoveEvent {
+                side: (Axis::X, piece_translation.x.round()),
+                rotate,
+            });
+        }
+    } else {
+        let delta_x = end_pos.x - start_pos.x;
+        let delta_y = end_pos.y - start_pos.y;
+        if delta_x.abs() > delta_y.abs() {
+            // x轴变化大，沿y轴旋转
+            let rotate = {
+                // 前面
+                if start_pos.z > 0.0 {
+                    if delta_x > 0.0 {
+                        SideRotation::Clockwise90
+                    } else {
+                        SideRotation::Counterclockwise90
+                    }
+                // 后面
+                } else {
+                    if delta_x > 0.0 {
+                        SideRotation::Counterclockwise90
+                    } else {
+                        SideRotation::Clockwise90
+                    }
+                }
+            };
+            return Some(SideMoveEvent {
+                side: (Axis::Y, piece_translation.y.round()),
+                rotate,
+            });
+        } else {
+            // y轴变化大，沿x轴旋转
+            let rotate = {
+                // 前面
+                if start_pos.z > 0.0 {
+                    if delta_y > 0.0 {
+                        SideRotation::Counterclockwise90
+                    } else {
+                        SideRotation::Clockwise90
+                    }
+                // 后面
+                } else {
+                    if delta_y > 0.0 {
+                        SideRotation::Clockwise90
+                    } else {
+                        SideRotation::Counterclockwise90
+                    }
+                }
+            };
+            return Some(SideMoveEvent {
+                side: (Axis::X, piece_translation.x.round()),
+                rotate,
+            });
+        }
+    }
+}
+
+pub fn choose_movable_pieces(
+    mut commands: Commands,
+    mut q_pieces: Query<(Entity, &Transform), With<Piece>>,
+    q_movable_pieces: Query<Entity, With<MovablePiece>>,
+    mut side_move_queue: ResMut<SideMoveQueue>,
+) {
+    if q_movable_pieces.is_empty() {
+        if side_move_queue.0.is_empty() {
+            return;
+        }
+        let event = side_move_queue.0.pop_front().unwrap();
+        dbg!(event);
+        for (_, transform) in &q_pieces {
+            info!(
+                "before choosing, cube translation={}",
+                transform.translation
+            );
+        }
+
+        let left_angle = match event.rotate {
+            SideRotation::Clockwise90 => FRAC_PI_2,
+            SideRotation::Clockwise180 => PI,
+            SideRotation::Counterclockwise90 => -FRAC_PI_2,
+        };
+
+        let side = event.side;
+        match side.0 {
+            Axis::X => {
+                for (entity, transform) in &mut q_pieces {
+                    if transform.translation.x == side.1 {
+                        info!("insert movable cube: translation={}", transform.translation);
+                        commands.entity(entity).insert(MovablePiece {
+                            axis: Axis::X,
+                            rotate: event.rotate,
+                            left_angle,
+                        });
+                    }
+                }
+            }
+            Axis::Y => {
+                for (entity, transform) in &mut q_pieces {
+                    if transform.translation.y == side.1 {
+                        info!("insert movable cube: translation={}", transform.translation);
+                        commands.entity(entity).insert(MovablePiece {
+                            axis: Axis::Y,
+                            rotate: event.rotate,
+                            left_angle,
+                        });
+                    }
+                }
+            }
+            Axis::Z => {
+                for (entity, transform) in &mut q_pieces {
+                    if transform.translation.z == side.1 {
+                        info!("insert movable cube: translation={}", transform.translation);
+                        commands.entity(entity).insert(MovablePiece {
+                            axis: Axis::Z,
+                            rotate: event.rotate,
+                            left_angle,
+                        });
+                    }
+                }
+            }
+        }
+    }
+}
+
+pub fn cleanup_movable_pieces(
+    mut commands: Commands,
+    movable_pieces: Query<(Entity, &MovablePiece)>,
+) {
+    for (entity, movable_piece) in &movable_pieces {
+        if movable_piece.left_angle == 0.0 {
+            commands.entity(entity).remove::<MovablePiece>();
+        }
     }
 }
